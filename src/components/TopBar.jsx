5d2431d5e92322ddefdbbd291d80e3ed9ff22785
Jaakko <jaakko.lipponen01@gmail.com>
2024-10-11 16:44:47 +0300

    tic tac toe half way point

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TopBar.css";
+
+const TopBar = () => {
+  return (
+    <div className="topbar">
+      <h1>TopBar text</h1>
+    </div>
+  );
+};
+
+export default TopBar;
